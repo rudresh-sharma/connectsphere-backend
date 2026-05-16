194396311300bf502a627e2c1e060dd6720c42a2
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-05-16 11:55:12 +0530

    [Rudresh] :stabilize auth-service tests with H2 test profile

--- auth-service/src/test/java/com/connectsphere/auth/controller/AuthControllerAuthenticatedTest.java
+++ auth-service/src/test/java/com/connectsphere/auth/controller/AuthControllerAuthenticatedTest.java
@@ -30,6 +30,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -43,6 +44,7 @@ import org.springframework.test.web.servlet.MockMvc;
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class AuthControllerAuthenticatedTest {
 
     @Autowired private MockMvc mockMvc;
--- auth-service/src/test/java/com/connectsphere/auth/controller/LoginRedirectControllerTest.java
+++ auth-service/src/test/java/com/connectsphere/auth/controller/LoginRedirectControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
@@ -18,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class LoginRedirectControllerTest {
 
     @Autowired private MockMvc mockMvc;
